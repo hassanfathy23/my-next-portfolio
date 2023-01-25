66649f05fb77fa17b58c9c69340647c89bb5dfc0
hassan <hassanfathy593@gmail.com>
2023-01-25 13:24:23 +0200

    changed work item

--- components/WorksList.tsx
+++ components/WorksList.tsx
@@ -6,35 +6,31 @@ function WorksList() {
       <WorkItem
         id={1}
         title="ecommerce for fashion"
-        description="i have built online store for clothing with full functionalities like \
-        payment, shipping and products, brands, categories, sub-categories listings. \
-        i used next.js, react.js, tailwindcss, redux-toolkit, mongodb, express.js to develop \
+        description="i have built online store for clothing with full functionalities like
+        payment, shipping and products, brands, categories, sub-categories listings.
+        i used next.js, react.js, tailwindcss, redux-toolkit, mongodb, express.js to develop
         this project in less than 45 days"
-        link="https://next-ecommerce-plum.vercel.app/"
-        image="https://res.cloudinary.com/oasis321/image/upload/v1667757982/ecommerce-backend/Screenshot_2022-10-26_103342_ugxs30.jpg"
+        link="https://next-ecommerce-plum.vercel.app"
+        image="https://res.cloudinary.com/oasis321/image/upload/v1674645505/Untitled_design_abmovj.png"
         from="sep 9 2022"
         to="oct 20 2022"
       />
       <WorkItem
         id={2}
-        title= "my 2nd portfolio"
-        description=
-          "i have built this portfolio because i felt that my previous portfolio was a little bit stale \
-          and i felt that i need a new one to display the quality that my clients should expect. \
-          i used next.js, react.js, tailwindcss and redux-toolkit for this project. \
-          i didn't implement any backend because this is just static website so far"
-        link= "/"
-        image=
-          "https://res.cloudinary.com/oasis321/image/upload/v1667761555/ecommerce-backend/Screenshot_2022-11-05_200712_thqd95.jpg"
+        title= "user interface for hotel"
+        description="user interface for a hotel&apos;s main page with customized design
+          and compatibility over all screen sizes, the stack used in this project
+          is next.js, react, tailwind, typescript"
+        link= "https://graceful-tanuki-18e88b.netlify.app"
+        image="https://res.cloudinary.com/oasis321/image/upload/v1674645241/Screenshot_2023-01-25_130541_ot3cck.jpg"
         from= "nov 2 2022"
         to= "nov 7 2022"
        />
        <WorkItem 
         id={3}
         title= "user interface for restaurant"
-        description=
-          "this is a user interface for a main page in a restaurant web application\
-          it&apos;s developed using top notch front end stack which includes: html, css, javascript, react, nextjs, tailwindcss, typescript and some ready-to-use components installed using npm\
+        description="this is a user interface for a main page in a restaurant web application
+          it&apos;s developed using top notch front end stack which includes: html, css, javascript, react, nextjs, tailwindcss, typescript and some ready-to-use components installed using npm
           this page is compatible with all kinds of devices(responsive) and very performant."
         link= "https://singular-fenglisu-fafd59.netlify.app"
         image="https://res.cloudinary.com/oasis321/image/upload/v1674029651/Screenshot_2023-01-18_080605_pyweon.jpg"
